e49cd3404642cf9a912f1bdd9eb855ea15b76eb6
Felipe <felipe_oliveira2019@hotmail.com>
2023-03-27 14:54:11 -0300

    adquirindo os valores das subtask

--- js/insertTask.js
+++ js/insertTask.js
@@ -6,15 +6,15 @@ class InsertTask {
         this.addSubTask = document.querySelector(addSubTask)
         this.modal = document.querySelector(modal)
         this.closeSub = []
+        this.subtaskValue = []
         this.allData = []
-        this.ObjectNote = {}
+        this.noteObjectPublic = {}
+
 
     }
     click = () => {
         this.set.addEventListener('click', () => {
             this.setRole()
-            this.allData += JSON.stringify(this.ObjectNote)
-            console.log(this.allData)
             this.modal.classList.remove('active')
 
         })
@@ -34,8 +34,8 @@ class InsertTask {
     // setar o note em sua posição no documento 
 
     setRole = () => {
-        this.ObjectNote.role = this.role.options[this.role.selectedIndex].value
-        const local = document.querySelector(`.${this.ObjectNote.role}`)
+        this.noteObjectPublic.role = this.role.options[this.role.selectedIndex].value
+        const local = document.querySelector(`.${this.noteObjectPublic.role}`)
         this.CreateInsertNote(local)
     }
 
@@ -47,6 +47,7 @@ class InsertTask {
         const subTaskPlace = document.createElement("div")
         const deleteSub = document.createElement("p")
         const subTaskLocale = document.querySelector(".subtasks")
+
 
 
         // setando seus atributos
@@ -64,6 +65,8 @@ class InsertTask {
         // adicionando a contagem de exclusão 
         this.quantiSubTask = document.querySelectorAll('.sub_place')
         this.closeSub = document.querySelectorAll('.deleta_sub')
+        this.subtaskValue = document.querySelectorAll(".subtask")
+
 
     }
 
@@ -83,32 +86,47 @@ class InsertTask {
     }
 
     SetInNote = (note) => {
-        // criando o ID na nota/note
-        let noteId = document.createElement('p')
-        noteId.value = Math.random().toFixed(2)
-            // criando o objeto da nota 
-        this.ObjectNote.title = document.querySelector("#title").value
+        // criando o objeto da nota publica e inserindo o title no objeto
+        this.noteObjectPublic.title = document.querySelector("#title").value
+        this.noteObjectPublic.description = document.querySelector("#description").value
+
 
         // adicionando o titulo na nota
         let NoteTitle = document.createElement('p')
-        NoteTitle.textContent = this.ObjectNote.title
+        NoteTitle.textContent = this.noteObjectPublic.title
         note.insertAdjacentElement("beforeend", NoteTitle)
 
-
+        // criando o ID na nota/note e inserindo no objeto note publico
+        let noteId = document.createElement('p')
+        noteId.value = Math.random().toFixed(2)
         noteId.innerText = noteId.value
         noteId.classList = "id"
-        this.ObjectNote.id = noteId.value
+        this.noteObjectPublic.id = noteId.value
         note.insertAdjacentElement("beforeend", noteId)
 
 
         // adicionando a quantidade de subtask na nota
         let NoteSubTask = document.createElement('p')
-        this.ObjectNote.subtaskCount = document.querySelectorAll(".subtask").length
-        NoteSubTask.textContent = `0 de ${this.ObjectNote.subtaskCount} subtask`
+        this.noteObjectPublic.subtaskCount = document.querySelectorAll(".subtask").length
+        NoteSubTask.textContent = `0 de ${this.noteObjectPublic.subtaskCount} subtask`
         note.insertAdjacentElement("beforeend", NoteSubTask)
-
+        this.factory(noteId.value, this.noteObjectPublic.title, this.noteObjectPublic.description, this.noteObjectPublic.subtaskCount)
+    }
+    GetSubtaskValue = () => {
+        this.subtaskValue.forEach((value, index) => {
+            console.log(value)
+        })
     }
 
+    // }
+
+    factory = (noteId, title, description, quantiSubtask) => {
+        let ObjectName = {}
+        ObjectName = { noteId, title, description, quantiSubtask }
+        this.allData.push(ObjectName)
+            // console.log(this.allData)
+            // console.log(this.noteObjectPublic)
+    }
     start = () => {
         this.click()
 
